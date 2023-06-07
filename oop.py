247e1add71297c72c5b5c210d3cc4cd544057b9e
EricFithian <ef2413@columbia.edu>
2023-06-06 20:41:14 -0400

    Updated with explicit passing of optional fields

--- oop.py
+++ oop.py
@@ -1,10 +1,10 @@
 class User:
-    def __init__(self, name = "Eric"):
-        self.name = name
+    def __init__(self, first_name = "Eric", last_name = "F"):
+        self.name = first_name + ' ' + last_name
 
     def greet(self):
         return print(f"Nice to meet you, {self.name}")
     
-eric = User("Chris")
+eric = User(first_name="Chris", last_name="S")
 print(eric.name)
 eric.greet()
